3c4df8f94d74533421e147059f94efc907058602
dongwoo kang <ehddn3821@naver.com>
2021-12-31 09:31:50 +0900

    Chore: Remove Pods

--- Pods/Target Support Files/Pods-FilmCanister-FilmCanisterUITests/Pods-FilmCanister-FilmCanisterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FilmCanister_FilmCanisterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FilmCanister_FilmCanisterUITests
-@end
--- Pods/Target Support Files/Pods-FilmCanister/Pods-FilmCanister-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FilmCanister : NSObject
-@end
-@implementation PodsDummy_Pods_FilmCanister
-@end
